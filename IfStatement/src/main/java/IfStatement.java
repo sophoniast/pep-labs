345b5a3c5ec9139fa71049db9e42ed955d03f06e
sophoniast <sophoniast@gmail.com>
2022-12-25 23:19:08 +0000

    Saved progress on IfStatement 2022-12-25 23:19:08 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -17,6 +17,7 @@ public class IfStatement {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
+        if booleanbool
         return 0;
     }
 }
