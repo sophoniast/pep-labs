034de571cffbf006723b1c33b9a7e19ff2ea742e
sophoniast <sophoniast@gmail.com>
2022-12-26 00:00:52 +0000

    Saved progress on Subtract 2022-12-26 00:00:52 +0000

--- Subtract/src/main/java/Subtract.java
+++ Subtract/src/main/java/Subtract.java
@@ -15,6 +15,7 @@ public class Subtract {
      * @return a minus b
      */
     public double sub(double a, double b){
-        return 0;
+        double sub = a - b;
+        return sub;
     }
 }
