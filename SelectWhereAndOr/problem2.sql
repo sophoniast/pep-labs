4e9aafcea57595c3a0dea4b83f3b8f460c01ad5e
sophoniast <sophoniast@gmail.com>
2023-01-14 02:17:54 +0000

    Saved progress on SelectWhereAndOr 2023-01-14 02:17:54 +0000

---
+++ SelectWhereAndOr/problem2.sql
@@ -0,0 +1,5 @@
+ /**
+         * Problem 2: Write a statement that will query the above table for all employees who earn more than $100,000 or less
+         * than $50,000
+         */
+SELECT * FROM employee WHERE salary > 100000 OR salary < 50000;
