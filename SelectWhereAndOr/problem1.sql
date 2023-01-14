77726e36b44eccc57700ff686ef4f139d4bb99e4
sophoniast <sophoniast@gmail.com>
2023-01-14 02:18:31 +0000

    Saved progress on SelectWhereAndOr 2023-01-14 02:18:31 +0000

--- SelectWhereAndOr/problem1.sql
+++ SelectWhereAndOr/problem1.sql
@@ -1,5 +1,1 @@
- /**
-         * Problem 1: Write a statement that will query the above table for all employees named 'Steve' who earn
-         * more than $75,000.
-         */
 SELECT * FROM employee WHERE first_name = 'Steve' AND salary > 75000;
--- SelectWhereAndOr/problem3.sql
+++ SelectWhereAndOr/problem3.sql
@@ -1,6 +1,1 @@
-/**
-         * Problem 3: Write a statement that will query the above table for all employees who earn more than $50,000 and are NOT
-         * named 'Steve'
-         * Hint: Look up the NOT and IN logical operators.
-         */
 SELECT * FROM employee WHERE salary > 50000 AND first_name != 'Steve';
