1445695c2be94929d348e40f4c23f8bb4d94825e
sophoniast <sophoniast@gmail.com>
2023-01-30 01:04:56 +0000

    Saved progress on ContainsDuplicates 2023-01-30 01:04:56 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,5 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +9,11 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        Set <Integer> dupl = new HashSet<Integer>();
+        for(int i=0; i<nums.length;i++){
+            if(!dupl.add(nums[i]))
+            return true;
+        }
         return false;
     }
 }
