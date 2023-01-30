f4e3df2572d6db364a3d7f0e4e5044d48b9e0542
sophoniast <sophoniast@gmail.com>
2023-01-30 00:44:59 +0000

    Saved progress on ContainsDuplicates 2023-01-30 00:44:59 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,6 +27,14 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        String s= "*";
+        for (int i = 0; i< n; i++){
+            for (int j=0; j<i; j++){
+                System.out.print("\n");
+                s += s;
+                System.out.print(s);
+            }
+        }
+        return s;
     }
 }
