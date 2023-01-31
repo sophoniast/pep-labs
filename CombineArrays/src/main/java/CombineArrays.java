a7628349d92b54a7355d330d483c61ac3348a98c
sophoniast <sophoniast@gmail.com>
2023-01-31 02:23:47 +0000

    Saved progress on CombineArrays 2023-01-31 02:23:47 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -12,13 +12,14 @@ public class CombineArrays {
     public int[] combine(int[] arr1, int[] arr2){
         int len = arr1.length + arr2.length;
         int [] arr3 = new int[len];
-        for(int i=0; i <arr1.length;i++){
+        int count = 0;
+        for(int i=0; i < arr1.length;i++){
             arr3[i]=arr1[i];
-            for(int j=(arr2.length+1);j<len;j++){
-                int count = 0;
-                arr3[-len]=arr2[count];
-            }
         }
+        for(int j=(arr1.length);j<len;j++){
+                arr3[j]=arr2[count];
+                count=count+1;
+            }
         return arr3;
     }
 }
